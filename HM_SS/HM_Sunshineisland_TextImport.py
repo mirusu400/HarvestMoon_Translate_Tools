99834321f549c88eb3e0ab31fa3af22f6d9ad75e
mirusu400 <mirusu400@naver.com>
2018-05-20 19:10:27 +0900

    HM_SS Files For Translation

---
+++ HM_SS/HM_Sunshineisland_TextImport.py
@@ -0,0 +1,145 @@
+# -*- coding:utf-8 -*-
+from time import sleep
+import sys
+import struct
+global outFp
+
+def string_hex_to_hex(temps):
+    for i in range(0, int(len(temps)/2)):
+        outtemp = int(temps[i * 2:i * 2 + 2], 16)
+        #print(temps[i * 2:i * 2 + 2])
+        outtemp2 = struct.pack("B", outtemp)
+        outFp.write(outtemp2)
+def tableread():
+    global TBLword
+    global TBLhex
+    inFp4=open(TBLfile,"r",encoding='utf-8')
+    while True:#
+        line=inFp4.readline()
+        line = line.replace(u"\ufeff", '')  # BOM고유오류 조정
+        line=str(line)
+        try:
+            tempa=line[-2]
+        except:
+            break
+        line=line.replace("\n","")
+        line=line.split("=")
+        if (line == ['']):
+            break
+        TBLword.append(tempa)
+        TBLhex.append(line[0])
+TBLword=[]
+TBLhex=[]
+REALTIVE_POINTER=[] #상대포인터 저장하는 공간입니다.
+MSG_NUMBER=[] #메시지 넘버 저장하는 공간입니다.
+DEF_LINE = 0 #총 라인 갯수를 셀때 실질적으로 저장되는 칸입니다.
+
+TBLfile="C:/3DP/TBLEdit.tbl"
+readfile=sys.argv[1]
+#readfile="C:/3DP/MessageData_0154.bin.txt"
+try:
+    writefile = sys.argv[2]
+except:
+    writefile=readfile
+    writefile+=".bin"
+tablefile="C:/3DP/TBLEdit.tbl"
+
+inFp = open(readfile, "r",encoding='utf-8')
+outFp=open(writefile,"wb")
+#result=inFp.read(0x04)
+tableread()
+line=[]
+line = inFp.readline()
+print(TBLword)
+k=0
+for i in range(len(line)):
+    try:
+        MSG_NUMBER.append(line[3+k : 3+k+4].upper()) #지금까지 메시지 넘버 파싱입니다.
+        k+=8
+        if(MSG_NUMBER[-1]==""):
+            MSG_NUMBER.remove(MSG_NUMBER[-1])
+            break
+    except:
+        break
+inFp.close()
+inFp = open(readfile, "r",encoding='utf-8')
+lines = 0
+lines=len(MSG_NUMBER) #총 대사 갯수
+DEF_LINE=lines
+
+inFp.seek(0)
+
+
+lines=str(hex(lines)) #총 라인수 쓰기시작..
+lines=lines[2:]
+if(len(lines)==3):
+    writelines=lines[1:] +"0" + lines[0] + "0000"
+elif(len(lines)==2):
+    writelines=lines + "000000"
+elif(len(lines)==1):
+    writelines=lines + "0000000"
+else:
+    print("총 라인수 에러!")
+writelines=writelines.upper()
+string_hex_to_hex(writelines) #총 라인수를 셉니다
+textlist=[]
+textlist=inFp.readlines()
+textlist[0:2] = [] #맨앞의 쓸모없는 라인 삭제
+for i in range(DEF_LINE):
+    templine=textlist[i]
+    templine=templine.replace("\n","") #모든 라인에서 엔터 항목을 제거합니다. 이후 00으로 채워줘야됨
+    textlist[i] = templine
+HEXline=[]
+for i in range(DEF_LINE): #코드들을 HEX로 바꿉니다.
+    templine=textlist[i]
+    templine=str(templine)
+    newline="" #헥스로 실제로 집어넣는 라인코드
+    for k in range(len(templine)):
+        Errorlevel = 1
+        for h in range(len(TBLword)):
+            if(templine[k] == TBLword[h]):
+                newline+=TBLhex[h]
+                Errorlevel=0
+                break
+        if(Errorlevel==1):
+            print("정의된 테이블이 없습니다!")
+            print(templine[k])
+            sleep(1000)
+    HEXline.append(newline)
+stackINT=0
+for i in range(DEF_LINE+1): #상대포인터(텍스트 길이) 삽입
+    try:
+        INTERVAL=int(len(HEXline[i])/2)+1+stackINT
+        stackINT=INTERVAL
+        INTERVAL=str(hex(INTERVAL))[2:]
+        if (len(INTERVAL) == 1):
+            INTERVAL = "0" + INTERVAL + "00"
+        elif (len(INTERVAL) == 2):
+            INTERVAL = INTERVAL + "00"
+        elif (len(INTERVAL) == 3):
+            INTERVAL = INTERVAL[1:]+ "0"+INTERVAL[0]
+        elif (len(INTERVAL) == 4):
+            INTERVAL = INTERVAL[2:] + INTERVAL[0:2]
+        INTERVAL=INTERVAL.upper()
+        string_hex_to_hex(str(INTERVAL))
+    except:
+        break
+print(MSG_NUMBER)
+for i in range(DEF_LINE):
+    templine=MSG_NUMBER[i]
+    print(templine)
+    insert1=templine[2:]
+    insert2=templine[0:2]
+    string_hex_to_hex(insert1.upper()) #메시지 넘버 삽입
+    string_hex_to_hex(insert2.upper())  # 메시지 넘버 삽입
+for i in range(DEF_LINE):
+    string_hex_to_hex(str(HEXline[i]))
+    string_hex_to_hex("00")
+outline=str(hex(outFp.tell()))[-1]
+
+if(outline == "1" or outline == "5" or outline == "9" or outline == "D"):
+    string_hex_to_hex("000000")
+elif(outline=="2" or outline == "6" or outline == "A" or outline == "E"):
+    string_hex_to_hex("0000")
+elif(outline=="3" or outline == "7" or outline == "B" or outline == "F"):
+    string_hex_to_hex("00")
---
+++ HM_SS/HM_Sunshineisland_Textextract.py
@@ -0,0 +1,169 @@
+# -*- coding:utf-8 -*-
+from time import sleep
+import sys
+
+
+global inFp
+def foundoffset():
+    blank=[]
+    inFp = open(readfile, "rb")
+    for i in range(4):
+        startpath=inFp.read(1)
+        temp=hex(ord(startpath))
+        if(temp=="0x0"):
+            temp="0x00"
+        blank.append(temp) #각각 읽어 blank에 추가
+    blank.reverse() #리틀엔디안->빅엔디안
+    pointer=""
+    pointer += blank[0]
+    pointer += blank[1]
+    pointer += blank[2]
+    pointer += blank[3]
+    pointer=pointer.replace("0x","")
+    result="0x"+pointer
+    print(result)
+    return int(result,16)
+def tableread():
+    global TBLword
+    global TBLhex
+    inFp4=open(TBLfile,"r",encoding='utf-8')
+    while True:
+        line=inFp4.readline()
+        line=line.replace("\n","")
+        line=line.split("=")
+        if (line == ['']):
+            break
+        line[0] = line[0].replace(u"\ufeff", '') #BOM고유오류 조정
+        TBLword.append(line[1])
+        TBLhex.append(line[0])
+TBLword=[]
+TBLhex=[]
+REALTIVE_POINTER=[] #상대포인터 저장하는 공간입니다.
+MSG_NUMBER=[] #메시지 넘버 저장하는 공간입니다.
+TBLfile="C:/3DP/TBL.tbl"
+#readfile=sys.argv[1]
+#readfile="C:/3DP/MessageData_0007.bin"
+readfile="C:/3DP/MessageData_0164.bin"
+try:
+    writefile = sys.argv[2]
+except:
+    writefile=readfile
+    writefile+=".txt"
+tablefile="C:/3DP/TBL.tbl"
+
+inFp = open(readfile, "rb")
+outFp=open(writefile,"w",encoding='utf-8')
+#result=inFp.read(0x04)
+print(readfile)
+tableread()
+OFFSET=foundoffset()
+print(OFFSET)
+
+inFp.read(0x04)
+for i in range(OFFSET):
+    blank=[]
+    for k in range(2): #2바이트씩 읽어야 합니다.
+        result = inFp.read(0x01)
+
+        temp = hex(ord(result))
+        if (temp == "0x0"):
+            temp = "0x00"
+        blank.append(temp)
+    blank.reverse()  # 리틀엔디안->빅엔디안
+    pointer = ""
+    pointer += blank[0]
+    pointer += blank[1]
+    pointer = pointer.replace("0x", "")
+    result = "0x" + pointer
+    REALTIVE_POINTER.append(result)
+temp=inFp.tell()
+for i in range(OFFSET): #메세지 넘버를 반환합니다.
+    blank = []
+    for k in range(2):  # 2바이트씩 읽어야 합니다.
+        result = inFp.read(0x01)
+        if (result == ''):
+            result = 0
+        temp = hex(ord(result))
+        if (temp == "0x0"):
+            temp = "0x00"
+        if (temp == "0x1"):
+            temp = "0x01"
+        if (temp == "0x2"):
+            temp = "0x02"
+        if (temp == "0x3"):
+            temp = "0x03"
+        if (temp == "0x4"):
+            temp = "0x04"
+        if (temp == "0x5"):
+            temp = "0x05"
+        if (temp == "0x6"):
+            temp = "0x06"
+        if (temp == "0x7"):
+            temp = "0x07"
+        if (temp == "0x8"):
+            temp = "0x08"
+        if (temp == "0x9"):
+            temp = "0x09"
+        if (temp == "0xa"):
+            temp = "0x0A"
+        if (temp == "0xb"):
+            temp = "0x0B"
+        if (temp == "0xc"):
+            temp = "0x0C"
+        if (temp == "0xd"):
+            temp = "0x0D"
+        if (temp == "0xe"):
+            temp = "0x0E"
+        if (temp == "0xf"):
+            temp = "0x0F"
+        blank.append(temp)
+    blank.reverse()  # 리틀엔디안->빅엔디안
+    pointer = ""
+    pointer += blank[0]
+    pointer += blank[1]
+    pointer = pointer.replace("0x", "")
+    result =  pointer
+    MSG_NUMBER.append(result)
+print(REALTIVE_POINTER)
+print(MSG_NUMBER)
+tblresult=''
+
+outFp.write(str(MSG_NUMBER))
+outFp.write("\n")
+
+
+for i in range(OFFSET):
+    tblresult = ''
+    while True:
+        s = inFp.read(0x01)
+        s = str(hex(ord(s)))
+        if (s == "0x0"):
+            s = "0x00"
+        if (s == "0x00"):
+            break
+        s = s.replace("0x", "")
+        tblresult+=s
+
+    tblresult=tblresult
+    for i in range((len(tblresult))):
+        try:
+            for k in range(len(TBLword)):
+                if (tblresult[i:i+2].upper() == TBLhex[k]):
+                    front=tblresult[:i]
+                    back=tblresult[i+2:]
+                    tblresult=front+TBLword[k]+back
+                    break
+                if (tblresult[i:i+4].upper() == TBLhex[k]):
+                    front=tblresult[:i]
+                    back=tblresult[i+4:]
+                    tblresult=front+TBLword[k]+back
+                    break
+        except:
+            break
+    print(tblresult)
+    outFp.write("\n")
+    outFp.write(str(tblresult))
+
+
+
+
